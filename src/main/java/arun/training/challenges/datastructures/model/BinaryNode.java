f4334fbdfc2651ba943d1baa43f05dc5800251b7
Arun K Nathan <arunknathan@gmail.com>
2018-10-18 10:12:11 +0530

    binary nodes

---
+++ src/main/java/arun/training/challenges/datastructures/model/BinaryNode.java
@@ -0,0 +1,33 @@
+package arun.training.challenges.datastructures.model;
+
+public class BinaryNode {
+
+	private int data;
+	private BinaryNode left;
+	private BinaryNode right;
+
+	public BinaryNode(int data) {
+		this.data = data;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setLeft(BinaryNode left) {
+		this.left = left;
+	}
+
+	public BinaryNode getLeft() {
+		return this.left;
+	}
+
+	public BinaryNode getRight() {
+		return this.right;
+	}
+
+	public void setRight(BinaryNode right) {
+		this.right = right;
+	}
+
+}
---
+++ src/test/java/arun/training/challenges/datastructures/model/BinaryNodeTest.java
@@ -0,0 +1,32 @@
+package arun.training.challenges.datastructures.model;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BinaryNodeTest {
+
+	@Test
+	public void testData() {
+		int data = 1;
+		BinaryNode node = new BinaryNode(data);
+		Assert.assertEquals(node.getData(), data);
+	}
+	
+	@Test
+	public void testLeft() {
+		BinaryNode node = new BinaryNode(1);
+		int data = 2;
+		BinaryNode left = new BinaryNode(data);
+		node.setLeft(left);
+		Assert.assertEquals(node.getLeft().getData(), data);
+	}
+	
+	@Test
+	public void testRight() {
+		BinaryNode node = new BinaryNode(1);
+		int data = 2;
+		BinaryNode right = new BinaryNode(data);
+		node.setRight(right);
+		Assert.assertEquals(node.getRight().getData(), data);
+	}
+}
